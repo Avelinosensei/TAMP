d743678a490e262318b77e107551c2b8fd3e37b2
Avelino Matos <57912181+Avelinosensei@users.noreply.github.com>
2020-12-15 20:07:37 +0000

    'primeira_pagina'

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function Home() {
+   return (
+      <div>
+      <h5>Home5</h5>
+      <contador/>
+      <div>Teste</div>
+   </div>
+   )
+
+}
+function contador() {
+   const [contador, setContador ]= useState(1);
+   function adicionarContador() {
+       setContador(contador + 1);
+
+   }
+   return (
+       <div>
+          <div>
+              {contador}
+          </div>
+           <button onClick={adicionarContador}>Adicionar</button>
+       </div>
+   )
+
+
+}
+export default Home
